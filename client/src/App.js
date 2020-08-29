1417308d9162c1dc63e24b14c8d816016846ed3e
Divyansh-P <70144022+Divyansh-P@users.noreply.github.com>
2020-08-29 14:08:38 +0530

    Gallery layout
    introduces a gallery layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import Home from './containers/Home/Home'
-import {Route,Switch} from 'react-router-dom'
+import Home from './containers/Home/Home';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Navbar from './containers/Navbar/Nav'
+import Navbar from './containers/Navbar/Nav';
+import Gallery from './containers/Gallery/Gallery';
 
 function App() {
   return (
-      <React.Fragment>
-        <Navbar/>
-        <Switch>
-        <Route path="/" component={Home} exact/>
-        </Switch>
-      </React.Fragment>
+    <React.Fragment>
+      <Navbar />
+      <Gallery />
+      <Switch>
+        <Route path="/" component={Home} exact />
+      </Switch>
+    </React.Fragment>
   );
 }
 
