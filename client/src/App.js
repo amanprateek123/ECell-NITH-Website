e9b19c12d32d09103a4077daa0a027ecb88c322e
Divyansh-P <70144022+Divyansh-P@users.noreply.github.com>
2020-08-29 14:45:40 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import Home from './containers/Home/Home'
-import {Route,Switch} from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import './App.css';
 import Navbar from './containers/Navbar/Nav'
+import Admin from './containers/Admin/Admin'
+
+
 
 function App() {
   return (
-      <React.Fragment>
-        <Navbar/>
-        <Switch>
-        <Route path="/" component={Home} exact/>
-        </Switch>
-      </React.Fragment>
+    <React.Fragment>
+      <Navbar />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/admin" component={Admin} />
+      </Switch>
+    </React.Fragment>
   );
 }
 
