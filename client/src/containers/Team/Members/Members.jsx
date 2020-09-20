0ab158835368c50e2b491e6c0c7279f568db7e5b
amanpra123 <amanpra333@gmail.com>
2020-09-20 18:13:07 +0530

    Team UI done

--- client/src/containers/Team/Members/Members.jsx
+++ client/src/containers/Team/Members/Members.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import classes from './Members.module.css'
-const members = (props) => (
+import FacebookIcon from '@material-ui/icons/Facebook';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+export default function members(props){
+    function typedArrayToURL(typedArray, mimeType) {
+        return URL.createObjectURL(new Blob([typedArray], {type: mimeType}))
+      }
+      let url = ''
+    function hi(arr){
+       const STRING_CHAR = props.team?arr.image.data.reduce((data, byte)=> {
+       return data + String.fromCharCode(byte);
+       }, ''):null
+       let base64String = btoa(STRING_CHAR);
+       url= ('data:image/jpg;base64,' + base64String)
+       return url
+    }
 
-    <div className={classes.container}>
-        <div >
-            <img src="https://picsum.photos/200" />
+    return(
+    <div className={classes.container1}>
+    <div className={classes.pics}  style={{backgroundImage:`url(${hi(props.team)})`}} >
+        <div className={classes.social} >
+             <FacebookIcon fontSize="inherit" style={{fontSize:'30px',cursor:'pointer'}} />
+             <LinkedInIcon fontSize="inherit" style={{fontSize:'30px',cursor:'pointer'}} />
         </div>
-        <h2>name</h2>
-        <h3> Post</h3>
     </div>
+    <h2 className={classes.name}>{props.team.name}</h2>
+    <h3 className={classes.post}>{props.team.post}</h3>
+    <p className={classes.email} >{props.team.email} </p>
+    
+</div>
+    )
 
-);
-export default members;
+};
--- client/src/containers/Team/Team.jsx
+++ client/src/containers/Team/Team.jsx
@@ -1,14 +1,35 @@
 import React from 'react'
 import './Team.scss'
 import Members from './Members/Members'
+import { useEffect,useState } from 'react'
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 
 export default function Team() {
+    const [team,setTeam] = useState([])
+    useEffect(()=>{
+        fetch('/team').then(res=>res.json()).then(res=>{
+            setTeam(res)
+          })
+    },[])
+    console.log(team)
     return (
         <React.Fragment>
-            <div>
-                Team
-        </div>
-            <Members />
+            <div className="team_head" >
+                E-Cell Team
+            </div>
+            <div className="row container m-auto" style={{marginRight:'0',height:'auto'}}>
+                {team.length?
+                team.map(item=>(
+                <div className="col-4 mb-4">
+                   <Members team={item} />
+                </div>
+                ))
+                :<div className="spin">
+                <CircularProgress/>
+              </div>}
+
+            </div>
         </React.Fragment>
     )
 }
