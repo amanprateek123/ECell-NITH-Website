3ce0fdf3175d70c3b9d05163ee2337e981cb6b09
Divyansh-P <70144022+Divyansh-P@users.noreply.github.com>
2020-09-27 13:30:51 +0530

    Vision section updated

---
+++ client/src/containers/Home/Vision/Vision.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import './Vision.scss';
+export default function Home() {
+    return (
+        <React.Fragment>
+            <h1 class="welcome">WELCOME TO ECELL-NITH</h1>
+            <div class="vision">
+
+                <div class="pop slide">
+                    <div class="image">
+                        <img src="https://picsum.photos/200" alt="img" />
+                    </div>
+                    <h2 class="heading">Vision</h2>
+                    <div class="note">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem rerum praesentium eligendi
+                        aliquid pariatur delectus repellat cumque porro neque ratione facilis mollitia, placeat magnam omnis
+                        dolorum est reprehenderit nisi eum! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                        Perferendis, officiis? Eius officiis illum, debitis sed enim omnis? Non temporibus laudantium aperiam,
+                        consectetur impedit repellendus tenetur iusto fugit reprehenderit dolor! Nemo. Lorem ipsum dolor sit
+                        amet consectetur adipisicing elit. Dolor saepe error aliquid minus fuga repellat soluta sequi tempora
+                        inventore, ullam iusto quis dignissimos eveniet maiores dicta molestias culpa autem maxime.
+            </div>
+                </div>
+                <div class="pop slide">
+                    <div class="image">
+                        <img src="https://picsum.photos/200" alt="img" />
+                    </div>
+                    <h2 class="heading">Vision</h2>
+                    <div class="note">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis labore magnam placeat quo debitis
+                        ipsum voluptas natus a laborum, eius velit laboriosam sequi vero, dolore sapiente nesciunt, ab ex!
+                        Deserunt?
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, totam, quasi voluptatibus
+                        saepe velit consectetur laborum minima provident atque accusantium magnam alias exercitationem excepturi
+                        praesentium, natus accusamus ullam voluptates corporis!
+            </div>
+                </div>
+                <div class="pop slide">
+                    <div class="image">
+                        <img src="https://picsum.photos/200" alt="img" />
+                    </div>
+                    <h2 class="heading">Vision</h2>
+                    <div class="note">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum perferendis molestias sunt sint sed totam
+                        esse iste vero architecto suscipit. Quas tempore quos deserunt molestias ipsam laborum eius doloribus
+                        voluptas.
+            </div>
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
