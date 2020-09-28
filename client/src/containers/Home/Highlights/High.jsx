13f8bad59add2188e6c5e25c895503ef14600748
amanpra123 <amanpra333@gmail.com>
2020-09-28 17:09:36 +0530

    Highlight event added in homepage

---
+++ client/src/containers/Home/Highlights/High.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './High.scss'
+
+export default function High() {
+    return (
+        <React.Fragment>
+            <h1 className="high">Highlight Events</h1>
+            <div className="highlight" >
+               <div className="back" >
+                  <div className="bg" >
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem rerum praesentium eligendi
+                        aliquid pariatur delectus repellat cumque porro neque ratione facilis mollitia, placeat magnam omnis
+                        dolorum est reprehenderit nisi eum! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                        Perferendis, officiis? Eius of
+                  </div>
+               </div>
+               <div className="back">
+               <div className="bg" >
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem rerum praesentium eligendi
+                        aliquid pariatur delectus repellat cumque porro neque ratione facilis mollitia, placeat magnam omnis
+                        dolorum est reprehenderit nisi eum! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                        Perferendis, officiis? Eius of
+                  </div>
+               </div>
+               <div className="back">
+               <div className="bg" >
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem rerum praesentium eligendi
+                        aliquid pariatur delectus repellat cumque porro neque ratione facilis mollitia, placeat magnam omnis
+                        dolorum est reprehenderit nisi eum! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                        Perferendis, officiis? Eius of
+                  </div>
+               </div>
+            </div>
+        </React.Fragment>
+    )
+}
--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Navbar/Nav'
 import Vision from './Vision/Vision'
+import Highlight from './Highlights/High'
 
 export default function Home() {
     return (
         <div>
           <Navbar/>
           <Vision/>
+          <Highlight/>
         </div>
     )
 }
