39aba4730629454ad82b0096a4aba098d1f7fd8c
amanpra123 <amanpra333@gmail.com>
2020-10-27 19:51:14 +0530

    Authority is added on homepage

--- client/src/components/Home/Authority/Authority.jsx
+++ client/src/components/Home/Authority/Authority.jsx
@@ -12,8 +12,26 @@ export default function Authority() {
     },[])
     console.log(auth)
     return (
-        <div>
-            Authority
+        auth.length>0?<div className="auth" >
+        <h2>Authority</h2>
+        <div className="faculty" >
+            <div className="div" data-aos="flip-left" data-aos-duration="1500" >
+                <img src={auth[0].image} />
+                <div>
+                <h4>{auth[0].name}</h4>
+                <p style={{color:'skyblue',fontWeight:'bold'}} >{auth[0].designation}</p>
+                <p>{auth[0].description}</p>
+                </div>
+            </div>
+            <div className="div" data-aos="flip-left" data-aos-duration="1500" >
+                <img src={auth[1].image} />
+                <div>
+                <h4>{auth[1].name}</h4>
+                <p style={{color:'skyblue',fontWeight:'bold'}} >{auth[1].designation}</p>
+                <p>{auth[1].description}</p>
+                </div>
+            </div>
         </div>
+    </div>:null
     )
 }
