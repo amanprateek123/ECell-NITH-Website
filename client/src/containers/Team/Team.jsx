fd59018fdd8d7cd591c3ab4e1bc2ca9027ca7748
amanpra123 <amanpra333@gmail.com>
2020-11-20 22:58:18 +0530

    Tab added in team

--- client/src/containers/Team/Team.jsx
+++ client/src/containers/Team/Team.jsx
@@ -8,6 +8,9 @@ import {NavLink} from 'react-router-dom'
 
 export default function Team() {
     const [team,setTeam] = useState([])
+    const [tab,setTab] = useState(0)
+
+
     useEffect(()=>{
         fetch('/team').then(res=>res.json()).then(res=>{
             setTeam(res)
@@ -20,7 +23,33 @@ export default function Team() {
             <div className="team_head" >
                 E-Cell Team
             </div>
-            <div className="m-2" style={{height:'2em'}} >
+            <div className="alumni" >
+               <div className="year" >
+                   <div onClick={()=>setTab(0)} className={tab===0?"tab":""} >
+                      Current Year
+                   </div>
+                   <div onClick={()=>setTab(1)} className={tab===1?"tab":""} >
+                      1999-2000
+                   </div>
+                   <div onClick={()=>setTab(2)} className={tab===2?"tab":""} >
+                      1999-2000
+                   </div>
+                   <div onClick={()=>setTab(3)} className={tab===3?"tab":""} >
+                      1999-2000
+                   </div>
+                   <div onClick={()=>setTab(4)} className={tab===4?"tab":""} >
+                      1999-2000
+                   </div>
+                   <div onClick={()=>setTab(5)} className={tab===5?"tab":""} >
+                      1999-2000
+                   </div>
+               </div>
+               <div className="list">
+                   
+               </div>
+                
+            </div>
+            {/*<div className="m-2" style={{height:'2em'}} >
                 <NavLink to='/alumni' style={{float:'right',margin:'0 1em',color:'skyblue',fontWeight:'bold',textDecoration:'none'}} >View Alumni Team</NavLink>
             </div>
             <div className="row container m-auto" style={{marginRight:'0',height:'auto'}}>
@@ -32,7 +61,7 @@ export default function Team() {
                 ))
                 }
 
-            </div>
+            </div>*/}
             </div>:<div className="spin">
                 <CircularProgress/>
               </div>}
