4473a07372774ce68123b7f6a813abf66c182934
amanpra123 <amanpra333@gmail.com>
2020-12-05 21:30:21 +0530

    build

---
+++ client/build/precache-manifest.21170754cc188cbd99f7df63546b6a01.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f2691bd793ff7a08ae259a1ea229f8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c38bac64df2d01e5e487",
+    "url": "/static/css/2.52e001b8.chunk.css"
+  },
+  {
+    "revision": "5fb2522ed1870669eb9e",
+    "url": "/static/css/main.743a6b60.chunk.css"
+  },
+  {
+    "revision": "c38bac64df2d01e5e487",
+    "url": "/static/js/2.853a1cab.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.853a1cab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fb2522ed1870669eb9e",
+    "url": "/static/js/main.7716e962.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "918531a661131cecfd97253cb30d4cf0",
+    "url": "/static/media/01.918531a6.jpg"
+  },
+  {
+    "revision": "a104af944e0c318bd99a8ccc10811fc6",
+    "url": "/static/media/02.a104af94.jpg"
+  },
+  {
+    "revision": "a011a0157faf1d67c450740cbbc2fe2d",
+    "url": "/static/media/03.a011a015.jpg"
+  },
+  {
+    "revision": "747d0a213ed764507e33bb4e3ee307f4",
+    "url": "/static/media/04.747d0a21.jpg"
+  },
+  {
+    "revision": "81430a722a4f9e1c9a1f6c6fc630d6de",
+    "url": "/static/media/eve.81430a72.jpg"
+  },
+  {
+    "revision": "72e01eedbab717ed5ca8b0a9ec79e1e7",
+    "url": "/static/media/eve1.72e01eed.jpg"
+  },
+  {
+    "revision": "0d5c94d62afb0b2a3c1ea6c29ff86db5",
+    "url": "/static/media/eve2.0d5c94d6.jpg"
+  }
+]);
