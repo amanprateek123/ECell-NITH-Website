b6c184a9c25dd5810aa99ae186b777721854925b
amanpra123 <amanpra333@gmail.com>
2021-02-13 17:11:36 +0530

    update

--- routes/ecell.js
+++ routes/ecell.js
@@ -3,15 +3,15 @@ const ctrl = require("../controllers/ecell");
 
 const router = new express.Router();
 
-router.get("/api/api/gallery", ctrl.getGallery);
-router.get("/api/api/gal", ctrl.getGal);
-router.get("/api/api/team", ctrl.getTeam);
-router.get("/api/api/initiative", ctrl.getEvent);
-router.get("/api/api/initiatives", ctrl.getEventInd);
-router.get("/api/api/auth", ctrl.getAuth);
-router.get("/api/api/partner", ctrl.getPartner);
-router.post("/api/api/getInTouch", ctrl.getInTouch);
-router.get("/api/api/blogs", ctrl.getBlog);
-router.get("/api/api/news", ctrl.getNews);
+router.get("/api/gallery", ctrl.getGallery);
+router.get("/api/gal", ctrl.getGal);
+router.get("/api/team", ctrl.getTeam);
+router.get("/api/initiative", ctrl.getEvent);
+router.get("/api/initiatives", ctrl.getEventInd);
+router.get("/api/auth", ctrl.getAuth);
+router.get("/api/partner", ctrl.getPartner);
+router.post("/api/getInTouch", ctrl.getInTouch);
+router.get("/api/blogs", ctrl.getBlog);
+router.get("/api/news", ctrl.getNews);
 
 module.exports = router;
